9a46a80fdc7724b66ade4c900d0e212bd44e2ec4
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-06-09 19:59:53 +0900

    feat: jwt 토근 생성 로직 구현 #12

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import clientRouter from "./routes/client.js";
+import authRouter from "./routes/auth.js";
 import { initWebSocket } from "./socket/socket.js";
+
+dotenv.config();
 
 const app = express();
 const port = 8080;
 
 // CORS 허용
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 // REST API 라우터 등록
 app.use("/", clientRouter);
+app.use("/auth", authRouter);
 
 // HTTP + WebSocket 서버 생성
 const server = http.createServer(app);
