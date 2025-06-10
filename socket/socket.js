2a4eb3615dfefca45a674a164c6d9b17fa8f4abc
Yun0825 <guddbs5545@naver.com>
2025-06-10 20:39:32 +0900

    test: Swift clientID 출력 테스트 #12

--- socket/socket.js
+++ socket/socket.js
@@ -12,6 +12,7 @@ export function initWebSocket(server) {
       try {
         const message = JSON.parse(data.toString());
         const { type, clientId } = message;
+        console.log(clientId ? "clientId" : "nothing");
 
         if (!clientId) return;
 
