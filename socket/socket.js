fba5958a59df5f5c71695d70af454f468e8235f6
Yun0825 <guddbs5545@naver.com>
2025-06-11 16:58:00 +0900

    comment: 불필요한 주석 삭제 #14

--- socket/socket.js
+++ socket/socket.js
@@ -12,8 +12,6 @@ export function initWebSocket(server) {
       try {
         const message = JSON.parse(data.toString());
         const { type, clientId } = message;
-        // console.log(type === "register-swift" ? `${clientId} ${message.pitch} ${message.yaw}` : "nothing");
-        // console.log(type === "type" ? `${clientId} ${message.pitch} ${message.yaw}` : "nothing");
 
         if (!clientId) return;
 
@@ -36,42 +34,47 @@ export function initWebSocket(server) {
 
           case "motion":
             if (pair.react) {
-              pair.react.send(JSON.stringify({
-                type: "motion",
-                pitch: message.pitch,
-                yaw: message.yaw,
-                name: message.name,
-                macBattery: message.macBattery,
-                airpodLeftBattery: message.airpodLeftBattery,
-                airpodRightBattery: message.airpodRightBattery
-              }));
+              pair.react.send(
+                JSON.stringify({
+                  type: "motion",
+                  pitch: message.pitch,
+                  yaw: message.yaw,
+                  name: message.name,
+                  macBattery: message.macBattery,
+                  airpodLeftBattery: message.airpodLeftBattery,
+                  airpodRightBattery: message.airpodRightBattery,
+                })
+              );
             }
             break;
 
           case "control":
             if (pair.swift) {
-              pair.swift.send(JSON.stringify({
-                type: "control",
-                name: message.name,
-                value: message.value
-              }));
+              pair.swift.send(
+                JSON.stringify({
+                  type: "control",
+                  name: message.name,
+                  value: message.value,
+                })
+              );
             }
             break;
 
           case "hotkey":
             if (pair.swift) {
-              pair.swift.send(JSON.stringify({
-                type: "hotkey",
-                name: message.name,
-                value: message.value
-              }));
+              pair.swift.send(
+                JSON.stringify({
+                  type: "hotkey",
+                  name: message.name,
+                  value: message.value,
+                })
+              );
             }
             break;
 
           default:
             console.warn("⚠️ 알 수 없는 타입:", type);
         }
-
       } catch (err) {
         console.error("❌ 메시지 처리 오류:", err.message);
         ws.send(JSON.stringify({ type: "error", message: err.message }));
