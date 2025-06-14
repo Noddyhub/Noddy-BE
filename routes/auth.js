880d4e29fc1180e114aab1c588102da891bb2f63
Yun0825 <guddbs5545@naver.com>
2025-06-14 20:15:46 +0900

    test: redirect_uri 생성시 userinfo.profile URL 추가 #16

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,8 @@ router.get("/google", (req, res) => {
         client_id: process.env.GOOGLE_CLIENT_ID,
         redirect_uri: process.env.GOOGLE_REDIRECT_URI,
         response_type: "code",
-        scope: "openid email profile",
+        scope:
+          "openid email profile https://www.googleapis.com/auth/userinfo.profile",
         access_type: "offline",
         prompt: "consent",
       }).toString();
