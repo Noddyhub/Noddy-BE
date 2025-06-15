1e085795babdddbf45cbf69e84173838cb994902
Yun0825 <guddbs5545@naver.com>
2025-06-15 13:19:11 +0900

    feat: MongoDB user 정보 schema 추가 #17

--- routes/client.js
+++ routes/client.js
@@ -1,20 +1,23 @@
 import express from "express";
-import Token from "../models/token";
+import UserToken from "../models/token";
 
 const router = express.Router();
 
-const tokenStore = new Map();
-
 router.post("/token", async (req, res) => {
-  const { callbackId, token } = req.body;
+  const { callbackId, token, name, email } = req.body;
   console.log("post callbackId:", callbackId, token);
 
-  if (!callbackId || !token) {
+  if (!callbackId || !token || !name || !email) {
     return res.status(400).json({ error: "callbackID or token이 없습니다." });
   }
 
   try {
-    const savedToken = await Token.create({ callbackId, token });
+    const savedToken = await UserToken.create({
+      callbackId,
+      token,
+      name,
+      email,
+    });
     console.log("토큰 저장 완료", savedToken);
 
     return res.status(201).json({ success: true });
@@ -29,13 +32,17 @@ router.get("/token/:callbackId", async (req, res) => {
   console.log("Confirm callbackId:", callbackId);
 
   try {
-    const tokenDoc = await Token.findOneAndDelete({ callbackId });
+    const tokenDoc = await UserToken.findOneAndDelete({ callbackId });
 
     if (!tokenDoc) {
       return res.status(404).json({ error: "Token이 없습니다." });
     }
 
-    return res.json({ token: tokenDoc.token });
+    return res.json({
+      token: tokenDoc.token,
+      name: tokenDoc.name,
+      email: tokenDoc.email,
+    });
   } catch (err) {
     console.error("토큰 조회 실패:", err);
     return res.status(500).json({ error: "서버 오류" });
