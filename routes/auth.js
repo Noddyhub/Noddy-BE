c7d120311df491b7280f653bebc30c354bc55a99
Yun0825 <guddbs5545@naver.com>
2025-06-15 11:35:52 +0900

    fix: URL 인식 오류로 인한 복구 #17

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,13 @@
 import express from "express";
 import axios from "axios";
 import { generateJWT } from "../utils/jwt.js";
-import { GOOGLE_OAUTH } from "../constants/constants.js";
 
 const router = express.Router();
 
 router.get("/google", (req, res) => {
   try {
     const redirect_uri =
-      GOOGLE_OAUTH.AUTH_BASE_URL +
+      "https://accounts.google.com/o/oauth2/v2/auth?" +
       new URLSearchParams({
         client_id: process.env.GOOGLE_CLIENT_ID,
         redirect_uri: process.env.GOOGLE_REDIRECT_URI,
@@ -20,6 +19,7 @@ router.get("/google", (req, res) => {
 
     console.log("[OAuth] 최종 redirect URL:", redirect_uri);
     res.redirect(redirect_uri);
+    console.log("[OAuth] Google 인증 페이지로 리다이렉트 완료");
   } catch (err) {
     console.error("[OAuth] redirect_uri 생성 중 에러 발생:", err);
     res.status(500).send("OAuth URL 생성 실패");
@@ -30,7 +30,7 @@ router.get("/google/callback", async (req, res) => {
   const code = req.query.code;
 
   try {
-    const tokenRes = await axios.post(GOOGLE_OAUTH.TOKEN_URL, {
+    const tokenRes = await axios.post("https://oauth2.googleapis.com/token", {
       code,
       client_id: process.env.GOOGLE_CLIENT_ID,
       client_secret: process.env.GOOGLE_CLIENT_SECRET,
@@ -40,15 +40,19 @@ router.get("/google/callback", async (req, res) => {
 
     const { access_token, id_token } = tokenRes.data;
 
-    const userInfo = await axios.get(GOOGLE_OAUTH.USER_INFO_URL, {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
+    const userInfo = await axios.get(
+      "https://openidconnect.googleapis.com/v1/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
 
-    const { email, name, sub: googleId } = userInfo.data;
+    const { email, name, sub: googleId, locale } = userInfo.data;
+    console.log(Object.keys(userInfo.data));
 
-    const token = generateJWT({ email, name, googleId });
+    const token = generateJWT({ email, name, googleId, locale });
 
     res.redirect(`${process.env.FRONTEND_URL}/oauth-success?token=${token}`);
   } catch (err) {
